99814b19017bb152b3a92c681f2544e0481f502b
Maxim <rqadchenkom@gmail.com>
2023-02-09 22:29:02 +0200

    Добавьте файлы проекта.

---
+++ Cafe administration/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe_administration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            bool isOpen = true;
+
+            Table[] tables = { new Table(1, 4), new Table(2, 8), new Table(3, 10) };
+
+            while (isOpen)
+            {
+                Console.WriteLine("Управління кафе.");
+
+                for (int i = 0; i < tables.Length; i++)
+                {
+                    tables[i].ShowInfo();
+                }
+
+                Console.Write("\nВведіть номер столу: ");
+                int wishTable = Convert.ToInt32(Console.ReadLine()) - 1;
+                Console.Write("\nВведіть кількість мість для забронювання: ");
+                int desiredPlaces = Convert.ToInt32(Console.ReadLine());
+
+                bool isReservationCompleted = tables[wishTable].Reserve(desiredPlaces);
+                if (isReservationCompleted)
+                {
+                    Console.WriteLine("Бронь пройшла успішно!");
+                }
+                else
+                {
+                    Console.WriteLine("Бронь не пройшла. Недостатньо місця.");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+
+    class Table
+    {
+        public int Number;
+        public int MaxPlaces;
+        public int FreePlaces;
+
+        public Table(int number, int maxPlaces)
+        {
+            Number = number;
+            MaxPlaces = maxPlaces;
+            FreePlaces = maxPlaces;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Стіл: {Number} вільно мість: {FreePlaces} з {MaxPlaces}.");
+        }
+
+        public bool Reserve(int places)
+        {
+            if(FreePlaces >= places)
+            {
+                FreePlaces -= places;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
